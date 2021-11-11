2f35af5e726f1ba9417322e81f9e0e920a53dddb
Vicente <lazvinz21@gmail.com>
2021-11-11 14:11:27 -0600

    the remove button function has been added to the project

--- e-shop/src/CheckoutProduct.js
+++ e-shop/src/CheckoutProduct.js
@@ -1,8 +1,18 @@
 import React from "react";
-import "./CheckoutProduct.css"
+import "./CheckoutProduct.css";
+import {useStateValue} from './StateProvider';
 
 function CheckoutProduct({id, image, title, price, rating})
 {
+    const [{basket}, dispatch] = useStateValue();
+
+    const removeFromBasket = () => {
+        dispatch({
+            type: "REMOVE_FROM_BASKET",
+            id: id,
+
+        })
+    }
     return(
             <>
                 <div className="checkoutProduct">
@@ -24,7 +34,7 @@ function CheckoutProduct({id, image, title, price, rating})
                             <p>⭐</p>
                         ))}
                 </div>
-                <button>Remove Item</button>
+                <button onClick={removeFromBasket}>Remove Item</button>
             </div>
             </>
     )
