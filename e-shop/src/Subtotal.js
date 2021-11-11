c218596d3f9b1321a5d1d6d8fc369f349ff37224
Vicente <lazvinz21@gmail.com>
2021-11-11 13:46:49 -0600

    adding basket functionalities

--- e-shop/src/Subtotal.js
+++ e-shop/src/Subtotal.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Subtotal.css";
 import CurrencyFormat from 'react-currency-format'; 
+import { useStateValue } from "./StateProvider"; 
+import {getBasketTotal} from "./reducer";
 
 function Subtotal(){
+    const [{basket}, dispatch] = useStateValue();
     return(
         <div className="subtotal">
             <CurrencyFormat
                 renderText ={(value)=>(
                     <>
                         <p>
-                            Subtotal(0 items): <stron>$0</stron>
+                            Subtotal({basket.length} items): <stron>${value}</stron> 
                         </p>
                         <small className="subtotal_gift">
                             <input type="checkbox" /> This order contains a gift
@@ -17,7 +20,7 @@ function Subtotal(){
                     </>
                 )}
                 decimalScale={2}
-                value={0}
+                value={getBasketTotal(basket)} //Total function
                 displayType={"text"}
                 thousandSeparator={true}
                 
